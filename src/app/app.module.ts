204b4916999d18f540087397d218abc94a0b1a41
Wayne <gigabyted224805@gmail.com>
2024-08-05 14:24:08 -0400

    v1 complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { LandingComponent } from './landing/landing.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +20,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, PortfolioComponent, LandingComponent]
 })
 export class AppModule { }
